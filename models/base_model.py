a9f4be8b98b99ba50ddbff895aa63923e4ea2763
Karenmiano <mianokaren1798@gmail.com>
2023-12-20 16:12:49 +0300

    connecting to db

--- models/base_model.py
+++ models/base_model.py
@@ -29,12 +29,12 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-        #if '_sa_instance_state' in self.__dict__:
-        #    del self.__dict__['_sa_instance_state']
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        if '_sa_instance_state' in self.__dict__:
+            del self.__dict__['_sa_instance_state']
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
