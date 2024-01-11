7e0ecd4d7f56388efba70d662cd38e0ad8a29f3b
Karenmiano <mianokaren1798@gmail.com>
2024-01-11 21:11:54 +0300

    deploy web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,22 +29,23 @@ def do_deploy(archive_path):
     """Deploy archive file to environment hosts"""
     if os.path.isfile(archive_path):
         with settings(warn_only=True):
-            fileregex = re.compile(r'.*/((.*)\.tgz)')
-            filefind = fileregex.search(archive_path)
-            file_name = filefind.group(1)
+            # fileregex = re.compile(r'.*/?((.*)\.tgz)')
+            # filefind = fileregex.search(archive_path)
+            # file_name = filefind.group(1)
+            file_name = archive_path.split('/')[-1]
             destination = f"/tmp/{file_name}"
-            # res1 = put(archive_path, destination)
-            decompress_to = f"/data/web_static/releases/{filefind.group(2)}/"
-            print(f"mv {decompress_to}web_static/* {decompress_to}")
-            # res2 = run(f"mkdir -p {decompress_to}")
-            # res3 = run(f"tar -xzf {destination} -C {decompress_to}")
-            # res4 = run(f"rm {destination}")
-            # res7 = run(f"mv {decompress_to}web_static/* {decompress_to}")
-            # res8 = run(f"rm -rf {decompress_to}web_static")
-            # res5 = run("rm -rf /data/web_static/current")
-            # res6 = run(f"ln -s {decompress_to} /data/web_static/current")
-            # results = [res1, res2, res3, res4, res5, res6, res7, res8]
-            # if all(result.succeeded for result in results):
-                # return True
-            # return False
-    # return False
+            res1 = put(archive_path, destination)
+            no_ext = file_name.split('.')[0]
+            decompress_to = f"/data/web_static/releases/{no_ext}/"
+            res2 = run(f"mkdir -p {decompress_to}")
+            res3 = run(f"tar -xzf {destination} -C {decompress_to}")
+            res4 = run(f"rm {destination}")
+            res7 = run(f"mv {decompress_to}web_static/* {decompress_to}")
+            res8 = run(f"rm -rf {decompress_to}web_static")
+            res5 = run("rm -rf /data/web_static/current")
+            res6 = run(f"ln -s {decompress_to} /data/web_static/current")
+            results = [res1, res2, res3, res4, res5, res6, res7, res8]
+            if all(result.succeeded for result in results):
+                return True
+            return False
+    return False
