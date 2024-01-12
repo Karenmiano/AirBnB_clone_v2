b05e7a485f2c87eb1a8152439229ed8b0fa33326
Karenmiano <mianokaren1798@gmail.com>
2024-01-12 13:54:29 +0300

    deploy archive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,7 +2,11 @@
 """Defines function do_pack"""
 
 from __future__ import with_statement
-from fabric.api import local, settings, put, run, sudo, env
+from fabric.api import settings
+from fabric.api import env
+from fabric.api import local
+from fabric.api import put
+from fabric.api import run
 from datetime import datetime
 import os.path
 import re
@@ -23,6 +27,30 @@ def do_pack():
         return None
     else:
         return archive_path
+
+
+def do_deploy(archive_path):
+    """Deploy archive file to environment hosts"""
+    if os.path.isfile(archive_path):
+        with settings(warn_only=True):
+            fileregex = re.compile(r'.*/?((.*)\.tgz)')
+            filefind = fileregex.search(archive_path)
+            file_name = filefind.group(1)
+            destination = f"/tmp/{file_name}"
+            res1 = put(archive_path, destination)
+            decompress_to = f"/data/web_static/releases/{filefind.group(2)}/"
+            res2 = run(f"mkdir -p {decompress_to}")
+            res3 = run(f"tar -xzf {destination} -C {decompress_to}")
+            res4 = run(f"rm {destination}")
+            res7 = run(f"mv -u {decompress_to}web_static/* {decompress_to}")
+            res8 = run(f"rm -rf {decompress_to}web_static")
+            res5 = run("rm -rf /data/web_static/current")
+            res6 = run(f"ln -s {decompress_to} /data/web_static/current")
+            results = [res1, res2, res3, res4, res5, res6, res7, res8]
+            if all(result.succeeded for result in results):
+                return True
+            return False
+    return False
 
 
 def deploy():
