0712c767bdd8cd02fd8b7c4ed2b48a6a3484e46f
Karenmiano <mianokaren1798@gmail.com>
2024-01-27 18:49:03 +0300

    requests to root

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hbnb():
-	"""Serves requests to the root"""
-	return "Hello HBNB!"
+    """Serves requests to the root"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
